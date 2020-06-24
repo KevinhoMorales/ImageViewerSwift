2df30ae672a68925fad3a7392d205e763e8446f1
Kevin Morales <kfmorales94@gmail.com>
2020-06-24 14:19:58 -0500

    First Upload

--- Image Viewer/ViewController.swift
+++ Image Viewer/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import ImageViewer_swift
 
 class ViewController: UIViewController {
-
+    
+    //@IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    lazy var imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "Icon")
+        
+        // Setup Image Viewer
+        iv.setupImageViewer()
+        return iv
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        view = UIView()
+        view.backgroundColor = .white
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20).isActive = true
+        imageView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    }
 
 }
 
